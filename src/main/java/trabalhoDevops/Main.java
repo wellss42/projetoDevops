d3af8d1a73cc1ae8c8807d81cbf3d744fc3a87fd
wellington martins <wellingtonmartins@MacBook-Pro-de-wellington.local>
2023-10-29 11:14:33 -0300

    adição de testes

--- src/main/java/trabalhoDevops/Main.java
+++ src/main/java/trabalhoDevops/Main.java
@@ -2,15 +2,21 @@ package trabalhoDevops;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		
-	}
-	
-	public int add(int a, int b) {
-		return a + b;
-	}
+    public static void main(String[] args) {
+        Main calculator = new Main();
 
-	public int subtract(int a, int b) {
-		return a - b;
-	}
+        int resultAdd = calculator.add(2, 3);
+        System.out.println("2 + 3 = " + resultAdd);
+
+        int resultSubtract = calculator.subtract(10, 5);
+        System.out.println("10 - 5 = " + resultSubtract);
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
--- src/test/java/trabalhoDevops/MainTest.java
+++ src/test/java/trabalhoDevops/MainTest.java
@@ -5,17 +5,73 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
 
-    @Test
-    public void testAdd() {
+	@Test
+    public void testAddSimple() {
         Main calculator = new Main();
-        int result = calculator.add(3, 5);
-        assertEquals(8, result);
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
     }
 
     @Test
-    public void testSubtract() {
-    	Main calculator = new Main();
-        int result = calculator.subtract(10, 4);
-        assertEquals(6, result);
+    public void testAddWithNegatives() {
+        Main calculator = new Main();
+        int result = calculator.add(-5, 3);
+        assertEquals(-2, result);
+    }
+
+    @Test
+    public void testAddWithZero() {
+        Main calculator = new Main();
+        int result = calculator.add(0, 7);
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void testAddWithLargeNumbers() {
+        Main calculator = new Main();
+        int result = calculator.add(1000000, 2000000);
+        assertEquals(3000000, result);
+    }
+
+    @Test
+    public void testSubtractSimple() {
+        Main calculator = new Main();
+        int result = calculator.subtract(10, 5);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testSubtractWithNegatives() {
+        Main calculator = new Main();
+        int result = calculator.subtract(-8, -3);
+        assertEquals(-5, result);
+    }
+
+    @Test
+    public void testSubtractWithZero() {
+        Main calculator = new Main();
+        int result = calculator.subtract(0, 7);
+        assertEquals(-7, result);
+    }
+
+    @Test
+    public void testSubtractWithLargeNumbers() {
+        Main calculator = new Main();
+        int result = calculator.subtract(1000000, 200000);
+        assertEquals(800000, result);
+    }
+
+    @Test
+    public void testSubtractResultingInNegative() {
+        Main calculator = new Main();
+        int result = calculator.subtract(5, 10);
+        assertEquals(-5, result);
+    }
+
+    @Test
+    public void testSubtractResultingInZero() {
+        Main calculator = new Main();
+        int result = calculator.subtract(7, 7);
+        assertEquals(0, result);
     }
 }
